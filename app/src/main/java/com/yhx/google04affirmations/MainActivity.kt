782681bcd5c5676bb034bcb860b5f25137d3b7fc
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-04-16 09:47:47 -0600

    Primer commit del proyecto.

---
+++ app/src/main/java/com/yhx/google04affirmations/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yhx.google04affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
