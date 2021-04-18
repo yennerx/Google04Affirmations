ab8c07468ee78d79f9af0330a11f051e7b9796b7
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-04-18 17:16:03 -0600

    se actualiza MainActivity.kt para usar el ItemAdapter en recyclerView

--- app/src/main/java/com/yhx/google04affirmations/MainActivity.kt
+++ app/src/main/java/com/yhx/google04affirmations/MainActivity.kt
@@ -2,12 +2,20 @@ package com.yhx.google04affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.yhx.google04affirmations.adapter.ItemAdapter
 import com.yhx.google04affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataSet = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.setHasFixedSize(true)//This setting is only needed to improve performance. Use this setting if you know that changes in content do not change the layout size of the RecyclerView.
     }
 }
