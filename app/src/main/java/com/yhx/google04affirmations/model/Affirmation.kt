31f1cf732e30f9d6d90bb161c4413ab094054e2f
Yenner Hernández <yenner.hernandez@hotmail.com>
2021-04-19 17:57:47 -0600

    Se agregan imagenes a los mensajes

--- app/src/main/java/com/yhx/google04affirmations/model/Affirmation.kt
+++ app/src/main/java/com/yhx/google04affirmations/model/Affirmation.kt
@@ -1,9 +1,15 @@
 package com.yhx.google04affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /*
 * Make Affirmation a data class by adding the data keyword before the class definition.
 * Add a val integer parameter stringResourceId to the constructor of the Affirmation class.
+* Modify the constructor of the Affirmation class by adding another Int parameter named imageResourceId.
 * */
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(
+        @StringRes val stringResourceId: Int,
+        @DrawableRes val imageResourceId: Int) {
 
 }
